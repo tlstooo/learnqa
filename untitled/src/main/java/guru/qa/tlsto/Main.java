fd9a53aa822e464a85da49394e451f4d15b165da
tlsto <frol54@gmail.com>
2025-04-09 22:37:51 +0300

    upd msg text

--- untitled/src/main/java/guru/qa/tlsto/Main.java
+++ untitled/src/main/java/guru/qa/tlsto/Main.java
@@ -4,14 +4,10 @@ package guru.qa.tlsto;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Hello there!");
 
         for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            System.out.println("iiiiii = " + i);
         }
     }
 }
